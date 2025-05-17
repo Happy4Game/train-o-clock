051994ea6ebf88e2bac2b0be20ac5ea0ee6d66c8
Happy4Game <dufeutrel.thibaut@gmail.com>
2025-05-17 21:27:51 +0200

    feat: Displaying a list of stoparea based on a string

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,62 +1,114 @@
-import { Image } from "expo-image";
-import { Button, StyleSheet, ToastAndroid } from "react-native";
+import {
+  Button,
+  FlatList,
+  Modal,
+  Text,
+  TextInput,
+  ToastAndroid,
+  View,
+} from "react-native";
 
-import ParallaxScrollView from "@/components/ParallaxScrollView";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
+import { useState } from "react";
+
+interface StopArea {
+  id: string;
+  embedded_type: string;
+  name: string;
+  quality: number;
+  stop_area: object;
+}
 
 export default function HomeScreen() {
-  const fetchTrainStation = async () => {
+  const [modalAddTrainVisible, setModalAddTrainVisible] = useState(false);
+  const [modalSelectStopAreaVisible, setModalSelectStopAreaVisible] =
+    useState(false);
+  const [town, setTown] = useState("");
+  const [stopAreas, setStopAreas] = useState<StopArea[]>([]);
+
+  const fetchTrainStation = async (query: string) => {
     try {
       const apiKey = await AsyncStorage.getItem("api-key");
       if (apiKey !== null) {
         const response = await axios.get(
-          "https://api.sncf.com/v1/coverage/sncf/places?display_geojson=false&q=Dun&",
+          `https://api.sncf.com/v1/coverage/sncf/places?display_geojson=false&q=${query}&`,
           { headers: { Authorization: apiKey } }
         );
-        const stopAreas: any[] = [];
-        response.data?.places.map((place: any) => {
-          if (place.id.startsWith("stop_area:")) stopAreas.push(place);
-        });
 
-        console.log(stopAreas);
+        const filtered = response.data?.places.filter((place: StopArea) =>
+          place.id.startsWith("stop_area:")
+        );
+        setStopAreas(filtered);
+        setModalSelectStopAreaVisible(true);
       }
     } catch (e) {
       console.error(e);
+      setModalAddTrainVisible(false);
+      setModalSelectStopAreaVisible(false);
       ToastAndroid.show("An error has been occured", ToastAndroid.SHORT);
     }
   };
 
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-      headerImage={
-        <Image
-          source={require("@/assets/images/partial-react-logo.png")}
-          style={styles.reactLogo}
-        />
-      }
-    >
-      <Button title="Add a Train" onPress={fetchTrainStation}></Button>
-    </ParallaxScrollView>
+    <View style={{ paddingTop: 100 }}>
+      <Button
+        title="Add a train"
+        onPress={() => setModalAddTrainVisible(!modalAddTrainVisible)}
+      />
+
+      <Modal
+        visible={modalAddTrainVisible}
+        animationType="slide"
+        onRequestClose={() => {
+          setModalAddTrainVisible(!modalAddTrainVisible);
+        }}
+      >
+        <View>
+          <TextInput
+            placeholder="Name of stop area"
+            value={town}
+            onChangeText={setTown}
+            style={{ color: "grey" }}
+          />
+          <Button
+            title="Fetch for"
+            onPress={() => {
+              fetchTrainStation(town);
+            }}
+          />
+          <Button
+            title="Close"
+            onPress={() => setModalAddTrainVisible(!modalAddTrainVisible)}
+          />
+        </View>
+      </Modal>
+
+      <Modal
+        visible={modalSelectStopAreaVisible}
+        animationType="slide"
+        onRequestClose={() => {
+          setModalSelectStopAreaVisible(!modalSelectStopAreaVisible);
+        }}
+      >
+        <View>
+          <FlatList
+            data={stopAreas}
+            renderItem={({ item }) => (
+              <Text style={{ marginBottom: 8 }}>{item.name}</Text>
+            )}
+            ListEmptyComponent={
+              <Text style={{ color: "gray" }}>No gare station found</Text>
+            }
+          />
+          <Button
+            title="Close"
+            onPress={() =>
+              setModalSelectStopAreaVisible(!modalSelectStopAreaVisible)
+            }
+          />
+        </View>
+      </Modal>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
-  },
-});
